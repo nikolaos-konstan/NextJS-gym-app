b3b8a6051e34a72871f57c13fbceb532ccfd6b04
nikolaos-konstan <nikolaos.konstan@gmail.com>
2023-11-26 10:48:35 +0200

    update

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,13 +1,26 @@
+import getUser from "@/lib/getUser";
+import getUserPosts from "@/lib/getUserPosts";
+import { Suspense } from "react";
+import UserPosts from "./components/UserPosts";
+
 type Params = {
   params: {
     userId: string;
   };
 };
 
-export default function UserPage({ params: { userId } }: Params) {
+export default async function UserPage({ params: { userId } }: Params) {
+  const userData: Promise<User> = getUser(userId);
+  const userPostsData: Promise<Post[]> = getUserPosts(userId);
+
+  const [user, userPosts] = await Promise.all([userData, userPostsData]);
   return (
     <>
-      <h1>UserPage Page</h1>
+      <h2>{user.name}</h2>
+      <br />
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <UserPosts promise={userPostsData} />
+      </Suspense>
     </>
   );
 }
